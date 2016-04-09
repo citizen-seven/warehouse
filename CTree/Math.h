c47bf52099f081a06379c12583b1e12366ed1df4
Nikita Mokrov <tismoney@mail.ru>
2016-04-09 15:32:13 +0300

    Fix tab and delete waste

--- CTree/Math.h
+++ CTree/Math.h
@@ -6,18 +6,18 @@
 
 class Math
 {
-	private:
-		string::iterator S;
-		string str;
-	public:
-		Math();
-		string GetEquation();
-		string PrepareEquation();
-		void PutStr(string st);
-		CNode* GetNum();                      //Num :: = ['0' - '9'] +
-		CNode* GetExp();                      //Exp :: = MulDiv {['+' '-']MulDiv}*
-		CNode* GetMulDiv();                   //MulDiv :: = Pas {['*' '/'] Pas}*
-		CNode* GetPas();                      //Pas:: = '(' Exp ')' | Num | F
+    private:
+        string::iterator S;
+        string str;
+    public:
+        Math();
+        string GetEquation();
+        string PrepareEquation();
+        void PutStr(string st);
+        CNode* GetNum();                      //Num :: = ['0' - '9'] +
+        CNode* GetExp();                      //Exp :: = MulDiv {['+' '-']MulDiv}*
+        CNode* GetMulDiv();                   //MulDiv :: = Pas {['*' '/'] Pas}*
+        CNode* GetPas();                      //Pas:: = '(' Exp ')' | Num | F
 };
 
 #endif
